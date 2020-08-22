4d78250499e527465327d6954bda489345fc22ef
AleksandrGogolushko <gogolushko@outlook.com>
2020-08-22 16:43:43 +0300

    add error-message

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-let currentLocation = 'firstPage';
+let currentPage = 'first';
 let autoScrolling = false;
 let firstHeight = $('.first').offset().top;
 let secondHeight = $('.second').offset().top;
@@ -7,19 +7,24 @@ let thirdHeight = $('.third').offset().top
 $(document).scroll(e => {
     let scrolled = $(window).scrollTop();
     if (!autoScrolling) {
-        if (scrolled > 1 && currentLocation == 'firstPage') {
-            scrollPage(secondHeight, 'secondPage');
-        } else if (scrolled > secondHeight && currentLocation == 'secondPage') {
-            scrollPage(thirdHeight, 'thirdPage');
-        } else if (scrolled < thirdHeight && currentLocation == 'thirdPage') {
-            scrollPage(secondHeight, 'secondPage');
-        } else if (scrolled < secondHeight && currentLocation == 'secondPage') {
-            scrollPage(firstHeight, 'firstPage');
+        switch (true) {
+            case scrolled > 1 && currentPage == 'first':
+                scrollPage(secondHeight, 'second');
+                break;
+            case scrolled > secondHeight && currentPage == 'second':
+                scrollPage(thirdHeight, 'third');
+                break;
+            case scrolled < thirdHeight && currentPage == 'third':
+                scrollPage(secondHeight, 'second');
+                break
+            case scrolled < secondHeight && currentPage == 'second':
+                scrollPage(firstHeight, 'first');
+                break
         }
     }
 
     function scrollPage(nextHeight, page) {
-        currentLocation = page;
+        currentPage = page;
         autoScrolling = true;
         $('body,html').animate({ scrollTop: nextHeight }, 600, () => {
             autoScrolling = false;
@@ -27,28 +32,62 @@ $(document).scroll(e => {
     }
 })
 
-$(".left").click((e) => { moveCarusel(e) })
-$(".right").click((e) => { moveCarusel(e) })
+let caruselSlide = 0;
+
+$("#left").click((e) => { moveCarusel(e) })
+
+$("#right").click((e) => { moveCarusel(e) })
 
 function moveCarusel(e) {
     let pos = $(document).width();
-    let position = $(".carusel-items").css("left")
-    if (e.target.classList[1] == "left") {
-        if (position != "0px") {
-            if (position == `${-pos * 2}px`) {
-                $(".carusel-items").animate({ left: `${(position.slice(0, -2)) / 2}` }, 100)
+    if ($(e.target).attr("id") == "left") {
+        if (caruselSlide != 0) {
+            if (caruselSlide == 2) {
+                $(".carusel-items").animate({ left: `${-pos}` }, 50)
+                caruselSlide = 1;
             } else {
-                $(".carusel-items").animate({ left: `${0}` }, 100)
+                $(".carusel-items").animate({ left: `${0}` }, 50)
+                caruselSlide = 0
             }
+            checkSelect("select", ".navigation", caruselSlide)
         }
     } else {
-        if (position == "0px") {
-            $(".carusel-items").animate({ left: `${-pos}` }, 100)
+        if (caruselSlide == 0) {
+            $(".carusel-items").animate({ left: `${-pos}` }, 50);
+            caruselSlide = 1;
+        } else if (caruselSlide == 1) {
+            $(".carusel-items").animate({ left: `${-pos * 2}` }, 50);
+            caruselSlide = 2;
         } else {
-            $(".carusel-items").animate({ left: `${-pos * 2}` }, 100)
+            $(".carusel-items").animate({ left: `${0}` }, 50);
+            caruselSlide = 0;
         }
+        checkSelect("select", ".navigation", caruselSlide)
     }
 }
+
+function checkSelect(addClass, perents, caruselSlide) {
+    $(`.${addClass}`).removeClass(addClass)
+    $(`${perents} div:nth-child(${caruselSlide + 1})`).addClass(addClass)
+}
+
+$(".first-block").click(() => {
+    $(".carusel-items").animate({ left: `${0}` }, 50)
+    caruselSlide = 0
+    checkSelect("select", ".navigation", caruselSlide)
+})
+
+$(".second-block").click(() => {
+    $(".carusel-items").animate({ left: `${-$(document).width()}` }, 50)
+    caruselSlide = 1;
+    checkSelect("select", ".navigation", caruselSlide)
+})
+
+$(".third-block").click(() => {
+    $(".carusel-items").animate({ left: `${-$(document).width() * 2}` }, 50);
+    caruselSlide = 2;
+    checkSelect("select", ".navigation", caruselSlide)
+})
 
 $(".second").mousemove(() => {
     moveStars(15)
@@ -60,44 +99,56 @@ function moveStars(speed) {
     $(".second img").css("transform", `translate(${x},${y})`)
 }
 
-let validationProgress = {
+let validationState = {
     name: false,
     email: false,
     message: false
 }
 
 $("#name").change(() => {
-    if (!$("#name").val().search(/[a-z]*[a-zA-Z][^\W*\d]{1,15}$/g)) {
-        validationProgress.name = true;
-        $("#name").css({ border: "1px solid green" })
-    } else {
-        validationProgress.name = false;
-        $("#name").css({ border: "1px solid crimson" })
-    }
+   validationInput("name", "email", /[a-z]*[a-zA-Z][^\W*\d]{1,15}$/g, "Name must contain only letters!")
 })
 
 $("#email").change(() => {
-    if (!$("#email").val().search(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/m)) {
-        validationProgress.email = true;
-        $("#email").css({ border: "1px solid green" })
-    } else {
-        validationProgress.email = false;
-        $("#email").css({ border: "1px solid crimson" })
-    }
+    validationInput("email", "name", /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/m, "Invalid email address. Valid e-mail can contain only latin letters, numbers, '@' and '.'!")
 })
+
+function validationInput(firstElemets, secondElements, regexp, errorText) {
+    if (!$(`#${firstElemets}`).val().search(regexp)) {
+        validationState[firstElemets] = true;
+        $(`.error-message.${firstElemets}`).attr("data-show", "hidden")
+        $(`#${firstElemets}`).css({ border: "1px solid green" })
+        $(`.${firstElemets}`).css({ transform: "translate(-50%, 10%)" })
+        if ($(`.error-message.${secondElements}`).attr("data-show") == "show") {
+            $(`.error-message.${secondElements}`).css({ transform: "translate(-50%, -110%)" })
+        }
+    } else {
+        validationState[firstElemets] = false;
+        $(`.error-message.${firstElemets}`).attr("data-show", "show")
+        $(`.error-message.${firstElemets} p`).html(errorText)
+        $(`#${firstElemets}`).css({ border: "1px solid crimson" })
+        $(`.error-message.${firstElemets}`).css({ transform: "translate(-50%, -110%)" })
+        if ($(`.error-message.${secondElements}`).attr("data-show") == "show") {
+            $(`.error-message.${secondElements}`).css({ transform: "translate(-50%, -220%)" })
+        }
+    }
+}
 
 $("#message").change(() => {
     if ($("#message").val().trim() == "") {
-        validationProgress.message = false;
+        validationState.message = false;
+        $("#message").css({ border: "1px solid crimson" })
     } else {
-        validationProgress.message = true;
+        validationState.message = true;
+        $("#message").css({ border: "1px solid green" })
     }
 })
 
 $("#send").click((e) => {
+    console.log(validationState)
     e.preventDefault()
-    for (let val in validationProgress) {
-        if (validationProgress[val] == false) {
+    for (let val in validationState) {
+        if (validationState[val] == false) {
             $(`#${val}`).css("transform", "translate(2%,0)"),
                 setTimeout(() => {
                     $(`#${val}`).css("transform", "translate(-2%,0)")
@@ -119,9 +170,9 @@ $(".modal button").click(() => {
 })
 
 function formRest() {
-    validationProgress.name = false;
-    validationProgress.email = false;
-    validationProgress.message = false;
+    validationState.name = false;
+    validationState.email = false;
+    validationState.message = false;
     $("#name,#email,#message").css({ border: "none" })
     $("form")[0].reset()
 }
